b8972a0fc6586f68044262284073c750762e1b28
Keshav-cyber <keshavpatidar7665@gmail.com>
2022-06-01 22:58:49 +0530

    nodemodule

---
+++ src/util/helper.js
@@ -0,0 +1,14 @@
+
+
+const printDate = function(){
+    console.log("1 june 2022")
+}
+const printMonth = function(){
+    console.log("june")
+}
+const getBatchInfo = function(){
+    console.log("radon w3d2 the topic for today is nodejs module system")
+}
+module.exports.printDate = printDate
+module.exports.printMonth = printMonth
+module.exports.getBatchInfo = getBatchInfo
---
+++ src/validator/formatter.js
@@ -0,0 +1,13 @@
+const trim = function(str){
+    console.log (str.trim())
+}
+const changeToLowerCase = function(str){
+      console.log (str.toLowerCase()) 
+}
+const changeToUpperCase = function(str){
+    console.log (str.toUpperCase())
+}
+
+module.exports.trim = trim
+module.exports.changeToLowerCase = changeToLowerCase
+module.exports.changeToUpperCase = changeToUpperCase
